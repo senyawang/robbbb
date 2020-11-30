d12ad84250c4bbe4781c2883994279bc075c79e1
wangyasen <wangyasen@jd.com>
2020-11-30 23:39:16 +0800

    dev

--- src/components/shop/cart.vue
+++ src/components/shop/cart.vue
@@ -14,64 +14,60 @@
 </template>
 
 <script>
-import ShoppingList from '../common/shoppingList.vue';
-import Button from "../common/Button.vue";
+import ShoppingList from '../common/shoppingList.vue'
+import Button from '../common/Button.vue'
 export default {
   data () {
     return {
       actionUrl: '',
       totalPrice: 0,
-      selected: [],
+      selected: []
     }
   },
   components: {
     ShoppingList,
-    Button,
+    Button
   },
   created () {
-       const localCart = JSON.parse(localStorage.getItem("CART")) || []
-        const totalPrice = localCart.reduce((a, c) => {
-          return a + c.price * c.number
-        }, 0)
+    const localCart = JSON.parse(localStorage.getItem('CART')) || []
+    const totalPrice = localCart.reduce((a, c) => {
+      return a + c.price * c.point
+    }, 0)
 
-        this.totalPrice = totalPrice
+    this.totalPrice = totalPrice
   },
   methods: {
-    changeHandle(id, value){
+    changeHandle (id, value) {
+      const localCart = JSON.parse(localStorage.getItem('CART')) || []
+      localCart.find(item => item.id === id).point = value
+      localStorage.setItem('CART', JSON.stringify(localCart))
 
-          const localCart = JSON.parse(localStorage.getItem("CART")) || []
-          localCart.find(item => item.id === id).number = value;
-          localStorage.setItem("CART", JSON.stringify(localCart))
-
-          this.updatePrice(this.selected);
-
+      this.updatePrice(this.selected)
     },
-    updatePrice(selected){
-       this.selected = selected;
-        const cartList = JSON.parse(localStorage.getItem("CART")) || [];
-        const checkedCart = cartList.filter(item => selected.includes(item.id))
-        const totalPrice = checkedCart.filter(item => item.checked).reduce((a, c) => {
-            return a + c.price * c.number
-          }, 0)
-        this.totalPrice = totalPrice
-        cartList.map(item => {
-          if(selected.includes(item.id)){
-            item.checked = true;
-          }
-        })
+    updatePrice (selected) {
+      this.selected = selected
+      const cartList = JSON.parse(localStorage.getItem('CART')) || []
+      const checkedCart = cartList.filter(item => selected.includes(item.id))
+      const totalPrice = checkedCart.filter(item => item.checked).reduce((a, c) => {
+        return a + c.price * c.point
+      }, 0)
+      this.totalPrice = totalPrice
+      cartList.map(item => {
+        if (selected.includes(item.id)) {
+          item.checked = true
+        }
+      })
 
-        localStorage.setItem("CART", JSON.stringify(cartList))
+      localStorage.setItem('CART', JSON.stringify(cartList))
     },
-    delHandle(id){
+    delHandle (id) {
+      const localCart = JSON.parse(localStorage.getItem('CART')) || []
+      const newCart = localCart.filter(item => item.id !== id)
+      localStorage.setItem('CART', JSON.stringify(newCart))
 
-        const localCart = JSON.parse(localStorage.getItem("CART")) || []
-        const newCart = localCart.filter(item => item.id !== id);
-        localStorage.setItem("CART", JSON.stringify(newCart))
-
-        this.updatePrice(this.selected);
-
+      this.updatePrice(this.selected)
     },
-    goToPay(){
+    goToPay () {
       this.$router.push('/robbbbuy/payDetail')
     }
   }
