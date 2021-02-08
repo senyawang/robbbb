563d2b2b33998aafc2eb10e7947f584ebf290c2c
wangyasen <wangyasen@jd.com>
2021-02-08 18:03:38 +0800

    dev

--- src/mobile/common/Button.vue
+++ src/mobile/common/Button.vue
@@ -47,7 +47,7 @@ export default {
   &.ghost {
     background: #fff;
     color: $black;
-    border: 0.5PX solid $black;
+    border: 0.8PX solid $black;
     &:hover {
       border-color: rgba($color: $black, $alpha: .5)
     }
